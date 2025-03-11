ade4bab1bfa66dfdf4857c203abd69324c04c787
ramlow07 <luamramlow@gmail.com>
2025-03-11 20:51:13 -0300

    fix: changing server port. port 5000 was already in use for frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,7 +23,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ error: "Server error" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
