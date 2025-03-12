1b94b830e84e18035067bc630efdb9c42ca1c5b7
ramlow07 <luamramlow@gmail.com>
2025-03-11 21:35:00 -0300

    feat: adding router into routes index

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,7 +4,11 @@ import { getHistoricalPrice } from "../controllers/priceController";
 import { getStats } from "../controllers/statsController";
 import { getVolumeData } from "../controllers/volumeController";
 
+const router = express.Router();
+
 router.get("/current", getPrice);
 router.get("/historical", getHistoricalPrice);
 router.get("/", getStats);
 router.get("/", getVolumeData);
+
+export default router;
