ccc7adb8d888e8e72b964a3fb4ffbe5df7c40748
ramlow07 <luamramlow@gmail.com>
2025-03-11 21:51:26 -0300

    wip: commenting statsController function. it'll be unused for now.

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { getPrice } from "../controllers/priceController";
 import { getHistoricalPrice } from "../controllers/priceController";
-import { getStats } from "../controllers/statsController";
+// import { getStats } from "../controllers/statsController";
 import { getVolumeData } from "../controllers/volumeController";
 
 const router = express.Router();
 
 router.get("/current", getPrice);
 router.get("/historical", getHistoricalPrice);
-router.get("/", getStats);
+// router.get("/", getStats);
 router.get("/", getVolumeData);
 
 export default router;
