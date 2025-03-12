276698ba2bbd1395571d706f327363bf6ccd23da
ramlow07 <luamramlow@gmail.com>
2025-03-11 23:22:52 -0300

    feature: feat: add watchlist routes for CRUD operations

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,11 +4,24 @@ import { getHistoricalPrice } from "../controllers/priceController";
 // import { getStats } from "../controllers/statsController";
 import { getVolumeData } from "../controllers/volumeController";
 
+import {
+  createWatchlist,
+  getWatchlist,
+  updateWatchlist,
+  deleteWatchlist,
+} from "../controllers/watchlistController";
+
 const router = express.Router();
 
 router.get("/current", getPrice);
 router.get("/historical", getHistoricalPrice);
 // router.get("/", getStats);
 router.get("/", getVolumeData);
+
+router.post("/watchlists", createWatchlist);
+router.get("/watchlists", getWatchlist);
+router.get("/watchlists/:id", getWatchlist);
+router.put("/watchlists/:id", updateWatchlist);
+router.delete("/watchlists/:id", deleteWatchlist);
 
 export default router;
